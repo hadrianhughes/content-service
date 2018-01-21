033af5b18179b2e361e941f4330ce0efa7702346
Hadrian Hughes <h.hughes@amaze.com>
2018-01-21 17:16:35 +0000

    Create initApi function

--- routes.js
+++ routes.js
@@ -6,55 +6,55 @@ const log = require('./lib/logger');
 router.get('/all', (req, res) => {
   res.status(200);
   res.end();
-  log.info({ route: 'all', status: 200 }, , 'Responded with 200');
+  log.info({ route: 'all', status: 200 }, 'Responded with 200');
 });
 
 router.get('/id/:id', (req, res) => {
   res.status(200);
   res.end();
-  log.info({ route: 'id', status: 200, params: req.params }, , 'Responded with 200');
+  log.info({ route: 'id', status: 200, params: req.params }, 'Responded with 200');
 });
 
 router.get('/uid/:uid', (req, res) => {
   res.status(200);
   res.end();
-  log.info({ route: 'uid', status: 200, params: req.params }, , 'Responded with 200');
+  log.info({ route: 'uid', status: 200, params: req.params }, 'Responded with 200');
 });
 
 router.get('/tag/:tag', (req, res) => {
   res.status(200);
   res.end();
-  log.info({ route: 'tag', status: 200, params: req.params }, , 'Responded with 200');
+  log.info({ route: 'tag', status: 200, params: req.params }, 'Responded with 200');
 });
 
 router.get('/date/:year/:month?/:day?', (req, res) => {
   res.status(200);
   res.end();
-  log.info({ route: 'date', status: 200, params: req.params }, , 'Responded with 200');
+  log.info({ route: 'date', status: 200, params: req.params }, 'Responded with 200');
 });
 
 router.get('/field/:field/:value', (req, res) => {
   res.status(200);
   res.end();
-  log.info({ route: 'field', status: 200, params: req.params }, , 'Responded with 200');
+  log.info({ route: 'field', status: 200, params: req.params }, 'Responded with 200');
 });
 
 router.get('/type/:type', (req, res) => {
   res.status(200);
   res.end();
-  log.info({ route: 'type', status: 200, params: req.params }, , 'Responded with 200');
+  log.info({ route: 'type', status: 200, params: req.params }, 'Responded with 200');
 });
 
 router.get('/type/:type/tag/:tag', (req, res) => {
   res.status(200);
   res.end();
-  log.info({ route: 'type-tag', status: 200, params: req.params }, , 'Responded with 200');
+  log.info({ route: 'type-tag', status: 200, params: req.params }, 'Responded with 200');
 });
 
 router.get('/type/:type/date/:year/:month?/:day?', (req, res) => {
   res.status(200);
   res.end();
-  log.info({ route: 'type-date', status: 200, params: req.params }, , 'Responded with 200');
+  log.info({ route: 'type-date', status: 200, params: req.params }, 'Responded with 200');
 });
 
 module.exports = router;
